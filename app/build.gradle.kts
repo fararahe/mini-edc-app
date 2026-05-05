00f73d439e6b234c3b464787385c27d06ba1b6ac
Faradyna Rahma <faradyna.rahma@gmail.com>
2026-05-05 23:59:27 +0700

    setup: initialize project and configure Hilt dependency injection

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "id.faradyna.miniedcapp"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "id.faradyna.miniedcapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    with(libs) {
+        implementation(platform(androidx.compose.bom))
+        implementation(bundles.compose)
+
+        implementation(androidx.activity.compose)
+        implementation(androidx.core.ktx)
+
+        testImplementation(junit)
+        androidTestImplementation(platform(androidx.compose.bom))
+        androidTestImplementation(androidx.compose.ui.test.junit4)
+        androidTestImplementation(androidx.espresso.core)
+        androidTestImplementation(androidx.junit)
+        debugImplementation(androidx.compose.ui.test.manifest)
+        debugImplementation(androidx.compose.ui.tooling)
+
+        // Hilt
+        implementation(hilt.android)
+        ksp(hilt.compiler)
+
+        // Coroutines
+        implementation(kotlinx.coroutines.android)
+
+        // Lifecycle
+        implementation(bundles.lifecycle)
+
+        // Datastore
+        implementation(androidx.datastore.preferences)
+
+        // Room
+        implementation(bundles.room)
+        ksp(androidx.room.compiler)
+    }
+}
---
+++ app/src/main/java/id/faradyna/miniedcapp/MiniEdcApp.kt
@@ -0,0 +1,7 @@
+package id.faradyna.miniedcapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MiniEdcApp: Application()
